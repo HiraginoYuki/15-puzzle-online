5d3d1f164a5fba742758c6ca613ba8c499bee137
kazukazu123123 <kazukazu.mein@gmail.com>
2021-06-30 18:50:00 +0900

    Update

--- src/components/SettingWindow/SettingWindow.tsx
+++ src/components/SettingWindow/SettingWindow.tsx
@@ -4,6 +4,12 @@ import { joinClass } from "../../join-class";
 import Close from "../Icon/Close";
 import styles from "./SettingWindow.module.scss";
 
+const LOCAL_STORAGE_UUID = "825a9cc1-022f-4858-9245-d19607d463fa";
+
+function saveSetting() {
+  window.localStorage.setItem(LOCAL_STORAGE_UUID, "aa");
+}
+
 export function SettingWindow() {
   const [showSettingsWindow, setShowSettingsWindow] = useGlobalState("showSettingsWindow");
   const closeSettingsWindow = useCallback(() => setShowSettingsWindow(false), []);
