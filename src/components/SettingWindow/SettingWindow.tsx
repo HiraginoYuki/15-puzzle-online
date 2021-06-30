acb941fcb5f308d60a1cb5362a5f1fa82b920695
kazukazu123123 <kazukazu.mein@gmail.com>
2021-06-30 19:53:36 +0900

    Update

--- src/components/SettingWindow/SettingWindow.tsx
+++ src/components/SettingWindow/SettingWindow.tsx
@@ -4,7 +4,7 @@ import { joinClass } from "../../join-class";
 import Close from "../Icon/Close";
 import styles from "./SettingWindow.module.scss";
 
-const LOCAL_STORAGE_UUID = "825a9cc1-022f-4858-9245-d19607d463fa";
+//const LOCAL_STORAGE_UUID = "825a9cc1-022f-4858-9245-d19607d463fa";
 
 /*function saveSetting() {
   window.localStorage.setItem(LOCAL_STORAGE_UUID, "aa");
