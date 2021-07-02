5be8068332ed7dab1a9ce248b839e003229a0a5d
kazukazu123123 <kazukazu.mein@gmail.com>
2021-07-02 21:24:09 +0900

    Update footer text

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
   return (
     <footer className={styles.footer}>
       <p className={styles.title}>Original:</p>
-      <a href="https://github.com/HiraginoYuki/15-puzzle.app" target="_blank"rel="noopener noreferrer">HiraginoYuki/15-puzzle.app</a>
+      <a href="https://github.com/HiraginoYuki/15-puzzle" target="_blank"rel="noopener noreferrer">HiraginoYuki/15-puzzle</a>
     </footer>
   );
 }
